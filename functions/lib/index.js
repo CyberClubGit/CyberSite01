5db770bd5f00016b456345eb5a42eb0baec4f1be
Samuel Belaisch <contact@cyber-club.net>
2025-11-21 17:34:20 +0000

    studio-4815326968:~/studio{main}$ firebase deploy --only functions:syncP

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,4 +1,3 @@
-
 "use strict";
 /**
  * Import function triggers from their respective submodules:
@@ -24,13 +23,23 @@ var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (
 }) : function(o, v) {
     o["default"] = v;
 });
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -137,10 +146,7 @@ exports.syncProductsFromSheet = functions.runWith({ secrets: [stripeSecretKey] }
                         sheet_id: productId,
                     },
                 };
-                const stripeProduct = await stripe.products.create({
-                    id: productId,
-                    ...stripeProductData
-                }).catch(async (error) => {
+                const stripeProduct = await stripe.products.create(Object.assign({ id: productId }, stripeProductData)).catch(async (error) => {
                     // Si le produit existe déjà (code: 'resource_already_exists'), on le met à jour
                     if (error.code === "resource_already_exists") {
                         functions.logger.log(`Product ${productId} already exists in Stripe, updating...`);
@@ -199,7 +205,7 @@ exports.syncProductsFromSheet = functions.runWith({ secrets: [stripeSecretKey] }
                     await pricesCollectionRef.doc(price.id).set({
                         active: price.active,
                         currency: price.currency,
-                        description: price.nickname,
+                        description: price.nickname, // Utilise nickname pour la description
                         type: price.type,
                         unit_amount: price.unit_amount,
                         metadata: price.metadata,
