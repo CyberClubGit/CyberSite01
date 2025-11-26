bef946519e9f4ce73ed594487ed0b8c933ce9ae5
Samuel Belaisch <contact@cyber-club.net>
2025-11-26 19:17:17 +0000

    Fix: Add Google User Content to Next.js image domains

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   output: "standalone",
   productionBrowserSourceMaps: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
